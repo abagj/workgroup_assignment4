b925bf6f50e7bd78e1fafec53c6decf8a673b6ee
Anikkenbg <anikkenbg@student.ie.edu>
2018-12-01 13:17:32 +0100

    Update exercise 3.py
    Execrise 3 , only the two first tests.

---
+++ exercise 3.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  1 12:17:40 2018
+
+@author: anikkengjeruldsen
+"""
+
+#%%
+
+#Excercise 3
+
+def find_path(graph, start, end, path=[]):
+
+        path = path + [start]
+        if start == end:
+            return path
+        if not graph.has_key(start):
+            return None
+        for node in graph[start]:
+            if node not in path:
+                newpath = find_path(graph, node, end, path)
+                if newpath: return newpath
+        return None
+
+
+def find_all_paths(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+        if not graph.has_key(start):
+            return []
+        paths = []
+        for node in graph[start]:
+            if node not in path:
+                newpaths = find_all_paths(graph, node, end, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+    
+    
