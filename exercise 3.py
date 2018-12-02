d3be41487b080bc93733adea5088c33080ef86a3
Florian Diegruber <florian@diegruber.net>
2018-12-02 13:16:46 +0100

    Adding Zinebs Input

--- exercise 3.py
+++ exercise 3.py
@@ -1,41 +1,95 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec  1 12:17:40 2018
+Created on Wed Nov 28 15:33:20 2018
 
-@author: anikkengjeruldsen
+@author: zinebmezzour
 """
-
 #%%
 
-#Excercise 3
+from Network_Path import find_path
+from Network_Path import find_all_paths
+from Network_Path import find_path_weighted
+from Network_Path import find_all_weighted_paths
 
-def find_path(graph, start, end, path=[]):
+#Graph exemples
+graph = {
+        "a":["b","c"],
+        "b": ["d"],
+        "c":["d"],
+        "d" : ["e"],
+        "e" :[],
+        "f":[]
+}
 
-        path = path + [start]
-        if start == end:
-            return path
-        if not graph.has_key(start):
-            return None
-        for node in graph[start]:
-            if node not in path:
-                newpath = find_path(graph, node, end, path)
-                if newpath: return newpath
-        return None
+graph2 = {
+        "a":["c"],
+        "b": ["d","a"],
+        "c":["d"],
+        "d" : ["e","f"],
+        "e" :[],
+        "f":[]
+}
 
+weighted_graph = {
+        "a":[{"node":"b","weight":1},{"node":"c","weight":2}],
+        "b": [{"node":"d","weight":3}],
+        "c":[{"node":"d","weight":1}],
+        "d" : [{"node":"e","weight":3}],
+        "e" :[],
+        "f":[]
+}
 
-def find_all_paths(graph, start, end, path=[]):
-        path = path + [start]
-        if start == end:
-            return [path]
-        if not graph.has_key(start):
-            return []
-        paths = []
-        for node in graph[start]:
-            if node not in path:
-                newpaths = find_all_paths(graph, node, end, path)
-                for newpath in newpaths:
-                    paths.append(newpath)
-        return paths
+#Find path 
+
+def value_one_path():
+    assert find_path(graph,"a","b") == ['a', 'b']
+    assert find_path(graph,"a","d") == ['a', 'b', 'd']
+    assert find_path(graph2,"b","c") == ['b', 'a', 'c']
+    assert find_path(graph2,"a","e")== ['a', 'c', 'd', 'e']
     
+   
+   
+def same_value_find_path():
+    assert find_path(graph,"a","a") == ['a']
+    assert find_path(graph,"b","b") == ['b']
     
+def none_value_find_path():
+     assert find_path(graph,"a","g") == None 
+     assert find_path(graph,"a","f") == None 
+     
+     
+     
+#All weighted     
+     
+
+def value_find_all_path():
+    assert find_all_paths(graph,"a","b") == [['a', 'b']]
+    assert find_all_paths(graph,"b","d") == [['b', 'd']]
+    assert find_all_paths(graph,"a","d") == [['a', 'b', 'd'], ['a', 'c', 'd']]
+    
+def no_value_find_all_path():
+    assert find_all_paths(graph,"b","c") == []
+    assert find_all_paths(graph,"d","a") == []
+
+
+#Path weighted
+
+
+def value_find_path_weighted():
+    assert find_path_weighted(weighted_graph,"a","b") == [{'node': 'a', 'weight': 0}, {'node': 'b', 'weight': 1}]
+    assert find_path_weighted(weighted_graph,"a","c") == [{'node': 'a', 'weight': 0}, {'node': 'c', 'weight': 2}]
+    assert find_path_weighted(weighted_graph,"b","d") == [{'node': 'b', 'weight': 0}, {'node': 'd', 'weight': 3}]
+
+
+    
+#ALL Path weighted
+
+def value_find_all_weighted_path():
+    assert find_all_weighted_paths(weighted_graph,"a","b") == [[{'node': 'a', 'weight': 0}, {'node': 'b', 'weight': 1}]]
+    assert find_all_weighted_paths(weighted_graph,"a","c") == [[{'node': 'a', 'weight': 0}, {'node': 'c', 'weight': 2}]]
+
+def same_value_find_all_weighted_path():
+    assert find_all_weighted_paths(weighted_graph,"a","a") == [[{'node': 'a', 'weight': 0}]]
+    assert find_all_weighted_paths(weighted_graph,"b","b") == [[{'node': 'b', 'weight': 0}]]
